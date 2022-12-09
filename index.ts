2fbc9b62807e892ae833192b9af3eff982f11f2b
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-09 14:41:33 +0800

    复制样例代码

--- index.ts
+++ index.ts
@@ -1,2 +1,18 @@
+const { Spot } = require('@binance/connector')
 
-console.log('你好，世界');
+const client = new Spot('', '', {
+  wsURL: 'wss://testnet.binance.vision' // If optional base URL is not provided, wsURL defaults to wss://stream.binance.com:9443
+})
+
+const callbacks = {
+  open: () => client.logger.log('open'),
+  close: () => client.logger.log('closed'),
+  message: data => client.logger.log(data)
+}
+const aggTrade = client.aggTradeWS('bnbusdt', callbacks)
+
+// unsubscribe the stream above
+setTimeout(() => client.unsubscribe(aggTrade), 3000)
+
+// support combined stream
+const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@ticker'], callbacks)

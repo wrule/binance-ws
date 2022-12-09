3ef633ad58d078b07d14404123b13bb9cdb420bc
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-09 14:58:34 +0800

    放开js

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const connector_1 = require("@binance/connector");
+const client = new connector_1.Spot('', '', {
+//   wsURL: 'wss://testnet.binance.vision' // If optional base URL is not provided, wsURL defaults to wss://stream.binance.com:9443
+});
+const callbacks = {
+    open: () => client.logger.log('open'),
+    close: () => client.logger.log('closed'),
+    message: (data) => client.logger.log(data)
+};
+const aggTrade = client.aggTradeWS('bnbusdt', callbacks);
+// unsubscribe the stream above
+// setTimeout(() => client.unsubscribe(aggTrade), 3000)
+// support combined stream
+// const combinedStreams = client.combinedStreams(['btcusdt@miniTicker', 'ethusdt@ticker'], callbacks)

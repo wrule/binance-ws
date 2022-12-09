587d2e637ff070e67c7b449e1ac3cd9c1c311900
jimao <jimao@jimaodeMacBook-Pro.local>
2022-12-09 15:22:30 +0800

    提交代码

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const connector_1 = require("@binance/connector");
-const client = new connector_1.Spot('', '', {
-//   wsURL: 'wss://testnet.binance.vision' // If optional base URL is not provided, wsURL defaults to wss://stream.binance.com:9443
-});
+const client = new connector_1.Spot('', '', {});
 const callbacks = {
-    open: () => client.logger.log('open'),
-    close: () => client.logger.log('closed'),
-    message: (data) => client.logger.log(data)
+    open: () => console.log('开始'),
+    close: () => console.log('结束'),
+    message: (data) => console.log(data),
 };
-const aggTrade = client.combinedStreams(['ethusdt@bookTicker'], callbacks);
+client.combinedStreams(['ethusdt@bookTicker'], callbacks);
 // unsubscribe the stream above
 // setTimeout(() => client.unsubscribe(aggTrade), 3000)
 // support combined stream
--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 import { Spot } from '@binance/connector';
 
-const client = new Spot('', '', {
-//   wsURL: 'wss://testnet.binance.vision' // If optional base URL is not provided, wsURL defaults to wss://stream.binance.com:9443
-});
+const client = new Spot('', '', { });
 
 const callbacks = {
-  open: () => client.logger.log('open'),
-  close: () => client.logger.log('closed'),
-  message: (data: any) => client.logger.log(data)
+  open: () => console.log('开始'),
+  close: () => console.log('结束'),
+  message: (data: any) => console.log(data),
 }
-const aggTrade = client.combinedStreams(['ethusdt@bookTicker'], callbacks);
+client.combinedStreams(['ethusdt@bookTicker'], callbacks);
 
 // unsubscribe the stream above
 // setTimeout(() => client.unsubscribe(aggTrade), 3000)
